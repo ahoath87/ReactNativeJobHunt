92be8e4e82380487a7b4a7825fef54661f5af153
Ashley Hoath <ahoath87@gmail.com>
2023-05-01 09:00:29 -0600

    this is the first commit

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+
+//this is a comment for git hub push
+// I need ot get and SSH key in here
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
